beb76c0397941f5d58ed54a72c5eb3d6bff28d95
cyokozai <yuu4suke27@outlook.jp>
2025-10-15 14:18:20 +0000

    Add Granim for canvas animations and update package dependencies

--- src/components/console.tsx
+++ src/components/console.tsx
@@ -1,8 +1,36 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import { css } from "~/styled-system/css";
 import { animate } from "motion";
+import Granim from "granim";
 
 export default component$(() => {
+  const canvasRef = useSignal<HTMLCanvasElement>();
+
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(({ track }) => {
+    track(() => canvasRef.value);
+
+    if (canvasRef.value) {
+      new Granim({
+        element: canvasRef.value,
+        direction: "left-right",
+        states: {
+          "default-state": {
+            gradients: [
+              ["#EB3349", "#F45C43"],
+              ["#FF8008", "#FFC837"],
+              ["#4CB8C4", "#3CD3AD"],
+              ["#24C6DC", "#514A9D"],
+              ["#FF512F", "#DD2476"],
+              ["#DA22FF", "#9733EE"],
+            ],
+            transitionSpeed: 2000,
+          },
+        },
+      });
+    }
+  });
+
   return (
     <div
       class={css({
@@ -52,23 +80,45 @@ export default component$(() => {
           cat title.vol2.txt
         </span>
       </div>
-      <span
+      <div
         class={css({
-          color: "#f9f9f9ff",
+          position: "relative",
+          width: "100%",
+          minHeight: "150px",
         })}
       >
-        &nbsp;&nbsp;____&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;____&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <br />
-        &nbsp;/&nbsp;___|&nbsp;&nbsp;&nbsp;_|&nbsp;|__&nbsp;&nbsp;&nbsp;___&nbsp;_&nbsp;__&nbsp;&nbsp;&nbsp;///&nbsp;&nbsp;&nbsp;///\\\&nbsp;&nbsp;|&nbsp;&nbsp;\/&nbsp;&nbsp;|_&nbsp;&nbsp;&nbsp;_&nbsp;___(_)&nbsp;___&nbsp;
-        <br />
-        |&nbsp;|&nbsp;&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;'_&nbsp;\&nbsp;/&nbsp;_&nbsp;\&nbsp;'__|&nbsp;///&nbsp;&nbsp;&nbsp;///&nbsp;&nbsp;\\\&nbsp;|&nbsp;|\/|&nbsp;|&nbsp;|&nbsp;|&nbsp;/&nbsp;__|&nbsp;|/&nbsp;__|
-        <br />
-        |&nbsp;|__|&nbsp;|_|&nbsp;|&nbsp;|_)&nbsp;|&nbsp;&nbsp;__/&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;\\\&nbsp;&nbsp;///&nbsp;&nbsp;&nbsp;///&nbsp;|&nbsp;|&nbsp;&nbsp;|&nbsp;|&nbsp;|_|&nbsp;\__&nbsp;\&nbsp;|&nbsp;(__&nbsp;
-        <br />
-        &nbsp;\____\__,&nbsp;|_.__/&nbsp;\___|_|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\\///&nbsp;&nbsp;&nbsp;///&nbsp;&nbsp;|_|&nbsp;&nbsp;|_|\__,_|___/_|\___|
-        <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|___/
-      </span>
+        <canvas
+          ref={canvasRef}
+          class={css({
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            display: "block",
+          })}
+        />
+        <span
+          class={css({
+            color: "#f9f9f9ff",
+            position: "relative",
+            display: "block",
+            mixBlendMode: "difference",
+          })}
+        >
+          &nbsp;&nbsp;____&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;____&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <br />
+          &nbsp;/&nbsp;___|&nbsp;&nbsp;&nbsp;_|&nbsp;|__&nbsp;&nbsp;&nbsp;___&nbsp;_&nbsp;__&nbsp;&nbsp;&nbsp;///&nbsp;&nbsp;&nbsp;///\\\&nbsp;&nbsp;|&nbsp;&nbsp;\/&nbsp;&nbsp;|_&nbsp;&nbsp;&nbsp;_&nbsp;___(_)&nbsp;___&nbsp;
+          <br />
+          |&nbsp;|&nbsp;&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;'_&nbsp;\&nbsp;/&nbsp;_&nbsp;\&nbsp;'__|&nbsp;///&nbsp;&nbsp;&nbsp;///&nbsp;&nbsp;\\\&nbsp;|&nbsp;|\/|&nbsp;|&nbsp;|&nbsp;|&nbsp;/&nbsp;__|&nbsp;|/&nbsp;__|
+          <br />
+          |&nbsp;|__|&nbsp;|_|&nbsp;|&nbsp;|_)&nbsp;|&nbsp;&nbsp;__/&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;\\\&nbsp;&nbsp;///&nbsp;&nbsp;&nbsp;///&nbsp;|&nbsp;|&nbsp;&nbsp;|&nbsp;|&nbsp;|_|&nbsp;\__&nbsp;\&nbsp;|&nbsp;(__&nbsp;
+          <br />
+          &nbsp;\____\__,&nbsp;|_.__/&nbsp;\___|_|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\\///&nbsp;&nbsp;&nbsp;///&nbsp;&nbsp;|_|&nbsp;&nbsp;|_|\__,_|___/_|\___|
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|___/
+        </span>
+      </div>
     </div>
   );
 });
