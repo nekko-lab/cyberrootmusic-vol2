e4ab11527b7240d0e1be56995e3f844e5de5f5a0
cyokozai <yuu4suke27@outlook.jp>
2025-11-01 19:43:43 +0000

    build: NGINX設定を更新し、/dj-liveパスのエイリアスを追加

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
 
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
+    base: "/dj-live/",
     plugins: [
       macroPlugin({ preset: "pandacss" }),
       qwikCity(),
